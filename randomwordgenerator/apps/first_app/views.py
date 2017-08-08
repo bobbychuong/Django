25ba0984c538e6398c26a4329b5dc8cfea86e288
Bobby <bobbychuong@gmail.com>
2017-08-08 16:22:01 -0700

    Random Word Generator

--- randomwordgenerator/apps/first_app/views.py
+++ randomwordgenerator/apps/first_app/views.py
@@ -1,18 +1,25 @@
 #imports
 from django.shortcuts import render, redirect
-import uuid
+from django.utils.crypto import get_random_string
+import random
+import string
+
 # Create your views here.
 def index(request):
+
     return render(request,'first_app/index.html')
 
-def random(request, string_length=10):
+
+def random(request):
+    unique_id = get_random_string(length=10)
     if 'count' in request.session:
         request.session['count'] += 1
+        request.session['word'] = unique_id
     else:
         request.session['count'] = 1
-
-    str = uuid.uuid4().hex
+        request.session['word'] = unique_id
     return render(request,'first_app/index.html')
+
 
 def reset_session(request):
     request.session.clear()
